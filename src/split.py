4f01abdfdf59545ff32f3478e6c70b92a655c588
sethnelson <sethnelson7525@gmail.com>
2025-10-18 09:26:53 -0400

    changed to 80/20 split as SKLearn uses CV-based methods

--- src/split.py
+++ src/split.py
@@ -7,13 +7,15 @@ processed_path = "../data/small_test_set/processed"
 X = np.load(processed_path + "/X.npy")
 y = np.load(processed_path + "/y.npy")
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
-X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.50)
+# *(SN) switching to 80/20 split since we'll use Cross Validation instead of manual validation
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+#X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.50)
 
 np.save(os.path.join(processed_path, "X_train.npy"), X_train)
 np.save(os.path.join(processed_path, "y_train.npy"), y_train)
-np.save(os.path.join(processed_path, "X_val.npy"), X_val)
-np.save(os.path.join(processed_path, "y_val.npy"), y_val)
+#np.save(os.path.join(processed_path, "X_val.npy"), X_val)
+#np.save(os.path.join(processed_path, "y_val.npy"), y_val)
 np.save(os.path.join(processed_path, "X_test.npy"), X_test)
 np.save(os.path.join(processed_path, "y_test.npy"), y_test)
 
