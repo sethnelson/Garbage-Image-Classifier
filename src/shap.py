c7e7ea50a52bebd98d45818729f13cbf86d08533
jpkolly <josephpkolly@gmail.com>
2025-11-18 22:40:04 -0500

    Shap analysis implementation

---
+++ src/shap.py
@@ -0,0 +1,19 @@
+import shap
+import numpy as np
+import torch
+
+#Shap implementation
+#Used: https://medium.com/@oveis/easy-guide-using-shap-algorithm-to-explain-cnn-classification-of-sar-images-mstar-database-8138657585c8
+
+def analyze_shap(model, Xtrain, Xtest, numBackground = 100, numSamples = 10):
+
+    #Sample background images
+    background = Xtrain[np.random.choice(Xtrain.shape[0], numBackground, replace = False)]
+
+    explainer = shap.DeepExplainer(model, background) # Initialize the deepexplainer
+    testSamples = Xtest[:numSamples] #Get test samples
+    shapValues = explainer.shap_values(testSamples) #Compute Shap
+    shap.image_plot(shapValues, testSamples) #plot Shap
+
+    return shapValues, explainer
+
